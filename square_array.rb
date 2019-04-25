a9ded537929f5ff7a0a49c6f2a9b8bdcc67243c0
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-25 22:32:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,5 @@
 def square_array(array)
-  # newArray = []
-  array = array.each { |x|  newArray.push( x * x ) }
-  # newArray
-  array
+  newArray = []
+  newArray = array.each { |x|  newArray.push( x * x ) }
+  newArray
 end
